58b030fc3374b5eee83189943132e6fe4b5ad803
Tanya <tanyairene.sheppard@triosstudent.com>
2017-07-22 17:16:47 -0400

    trying to figure out figaro without help

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV['pk_test_aIC7I2nZ7gXGmA0nIIizga4c'],
-  :secret_key      => ENV['sk_test_uR2m3dqPerFvLztOqT7lMjQ0']
+  :publishable_key => ENV['PUBLISHABLE_KEY'],
+  :secret_key      => ENV['SECRET_KEY']
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
